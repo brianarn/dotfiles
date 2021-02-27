20c966a2dbcb2ddccedfac2f14ee3688dc50ca43
Brian Sinclair <brianarn@gmail.com>
2021-02-27 06:33:03 +0000

    Automating Spaceship install

--- update_submodules.sh
+++ update_submodules.sh
@@ -2,6 +2,10 @@
 printf "Updating all submodules to master branch...\n"
 git submodule foreach 'git checkout master && git pull origin master'
 
+printf "\nSetting up Spaceship theme...\n"
+EXTROOT="$HOME/.dotfiles/external"
+ln -s "$EXTROOT/spaceship-prompt/spaceship.zsh-theme" "$EXTROOT/oh-my-zsh/custom/themes/spaceship.zsh-theme"
+
 printf "\nInstalling fzf ...\n"
 external/fzf/install --all
 
